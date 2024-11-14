6f847087fee2f5352ab1bf3bb0a3f5b63fadd7ba
SalmaHagi <saluhagi@gmail.com>
2024-11-14 09:32:19 +0300

    finished styling

--- my-app/src/components/Instructions.js
+++ my-app/src/components/Instructions.js
@@ -1,7 +1,7 @@
 const Person = (props) => {
     return (
         <> 
-            <h1>Step: {props.step}</h1>
+            <h1 className="inst">Step: {props.step}</h1>
             <h2>Title: {props.title}</h2>
             <h2>Content: {props.content}</h2>
         </>
@@ -14,7 +14,8 @@ const Instructions = () => {
             <Person 
                 step={1} 
                 title="Vocabulary and Set Up" 
-                content="You will see two rows of six (pits), with a long (mancala) on each end. The board is divided into two parts: Your side and mancala, and your opponent's side and mancala. Your side is the six pits closest to you, and your mancala is to your right. The same is for your opponent; their side is the six pits closest to them and their mancala is to their right."
+                content="You will see two rows of six (pits),
+                 with a long (mancala) on each end. The board is divided into two parts: Your side and mancala, and your opponent's side and mancala. Your side is the six pits closest to you, and your mancala is to your right. The same is for your opponent; their side is the six pits closest to them and their mancala is to their right."
             />
             <Person 
                 step={2} 
@@ -32,6 +33,7 @@ const Instructions = () => {
                 content="If, when dropping stones in pits, you drop a stone into your own mancala and that is the last stone in your hand, then you get to go again. If, when dropping stones into pits, you drop a stone into a pit that was previously empty, and the pit is on your side, AND that was the last stone in your hand, you take all of the stones in the pit directly above, and place them into your mancala."
             />
             <Person 
+                step={5}
                 title="Good Luck!" 
                 content="Now you are ready to play Mancala! Have fun and strategize wisely."
             />
